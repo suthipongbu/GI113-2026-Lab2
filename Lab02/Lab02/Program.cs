62083384f82b57a8b014497a345da6cd715fb20c
suthipongbu <stpx007z@gmail.com>
2026-08-31 16:59:02 +0700

    Update Program.cs

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -2,7 +2,6 @@
 // Name: สุธิพงษ์ ชาวนาแฝง
 // Section: -
 // No.: -
-
 using System;
 
 // ==================== Part A ====================
@@ -54,22 +53,20 @@ bool heroAlive = true;
 string mageName = "Luna";
 int mageHp = 120;
 double mageMana = 95.5;
-float magePower = 38.5f;
-char mageRank = 'B';
+char mageRank = 'S';
+bool mageAlive = true;
 
 string warriorName = "Rex";
 int warriorHp = 250;
 float warriorAttack = 55.5f;
-double warriorDefense = 30.25;
-bool warriorAlive = true;
 char warriorRank = 'S';
+bool warriorAlive = true;
 
 string healerName = "Mira";
 int healerHp = 140;
-double healerHealPower = 72.75;
 float healerSpeed = 8.5f;
-bool healerAlive = true;
 char healerRank = 'A';
+bool healerAlive = true;
 
 Console.WriteLine($"Name: {heroName}");
 Console.WriteLine($"HP: {heroHp}");
@@ -80,19 +77,17 @@ Console.WriteLine($"Alive: {heroAlive}");
 Console.WriteLine($"Name: {mageName}");
 Console.WriteLine($"HP: {mageHp}");
 Console.WriteLine($"Mana: {mageMana}");
-Console.WriteLine($"Power: {magePower}");
 Console.WriteLine($"Rank: {mageRank}");
+Console.WriteLine($"Alive: {mageAlive}");
 
 Console.WriteLine($"Name: {warriorName}");
 Console.WriteLine($"HP: {warriorHp}");
 Console.WriteLine($"Attack: {warriorAttack}");
-Console.WriteLine($"Defense: {warriorDefense}");
-Console.WriteLine($"Alive: {warriorAlive}");
 Console.WriteLine($"Rank: {warriorRank}");
+Console.WriteLine($"Alive: {warriorAlive}");
 
 Console.WriteLine($"Name: {healerName}");
 Console.WriteLine($"HP: {healerHp}");
-Console.WriteLine($"Heal Power: {healerHealPower}");
 Console.WriteLine($"Speed: {healerSpeed}");
-Console.WriteLine($"Alive: {healerAlive}");
 Console.WriteLine($"Rank: {healerRank}");
+Console.WriteLine($"Alive: {healerAlive}");
